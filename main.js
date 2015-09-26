49d953fde5ab466cd107cc42f7a8f1144df38d73
David Karn <davidkarnseattle@gmail.com>
2015-09-26 12:19:12 -0700

    add leaflet map w/openstreetmaps

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var vacant_lots             = 'mdtz-kt2d';
 var employment_stats        = '6bn5-2933';
 var liquor_licenses         = 'mjvz-43ce';
 var kenmore_zip             = 98028;
+var kenmore_coords          = [47.752778, -122.247222];
+var mapbox_id               = 'davidkarn.ni38i7gd';
+var mapbox_access_token     = ('pk.eyJ1IjoiZGF2aWRrYXJuIiwiYSI6ImNpZjFmdXlpYzBmbDB'
+                               + 'zcW01bDF3b3kyMTIifQ.-ilX5JP9cl1MrPMQUZJ6Gw');
+var map;
 
 function lookup(resource, query, next, error) {
     var query_params = {};
@@ -30,4 +35,18 @@ function in_kenmore_parcel(query) {
     query.where.push('zip_code = ' + kenmore_zip);
     return query; }
 
+function init_map() {
+    map = L.map('map')
+        .setView(kenmore_coords, 13);
+    // L.tileLayer('https://api.tile.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+    //             {attribution:  '',
+    //              maxZoom:      18,
+    //              id:           mapbox_id,
+    //              accessToken:  mapbox_access_token})
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                {attribution: 'osm'})
+        .addTo(map);
+    return map; }
 
+$(document).ready(function() {
+    init_map(); });
