646b2e0824d2849073f22693e48880e6aaf13502
David Karn <davidkarnseattle@gmail.com>
2015-09-26 14:54:28 -0700

    filter by commercial building type

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ var kenmore_coords          = [47.752778, -122.247222];
 var mapbox_id               = 'davidkarn.ni38i7gd';
 var mapbox_access_token     = ('pk.eyJ1IjoiZGF2aWRrYXJuIiwiYSI6ImNpZjFmdXlpYzBmbDB'
                                + 'zcW01bDF3b3kyMTIifQ.-ilX5JP9cl1MrPMQUZJ6Gw');
-var layers                  = {};
 var map;
+var building_data;
+var building_types          = [];
+var layers                  = {};
 var saved_coords            = {};
 var heatmap_layer_options   = 
         {vacancies:  {},
@@ -55,15 +57,21 @@ function socrata_vacant_lots_latlngs(next) {
 function socrata_latlngs(dataset, next) {
     lookup(dataset, in_kenmore(),
            function(buildings) {
-               var coords = buildings.map(function(building) {
-                   return [parseFloat(building.latitude), parseFloat(building.longitude)]; });
+               if (dataset == commercial_buildings) {
+                   building_data   = buildings;
+                   filter_commercial_buildings(); }
+               var coords = socrata_coords(buildings);
                next(coords); }); }
+
+function socrata_coords(buildings) {
+    var coords = buildings.map(function(building) {
+        return [parseFloat(building.latitude), parseFloat(building.longitude)]; });
+    return coords; }
                
 function plot_coords_heat(coords, layer) {
-    saved_coords[layer] = coords;
-    console.log(coords, layer);
-    layer           = layer || Math.random().toString();
-    var map_layer   = layers[layer];
+    saved_coords[layer]   = coords;
+    layer                 = layer || Math.random().toString();
+    var map_layer         = layers[layer];
     
     if (map_layer) {
         map_layer.setLatLngs(coords);
@@ -73,13 +81,36 @@ function plot_coords_heat(coords, layer) {
         layers[layer]   = L.heatLayer(coords, options);
         layers[layer].addTo(map); }}
 
+function filter_commercial_buildings() {
+    building_data.map(function(b) {
+        building_types.push({label: b.predominant_use}); });
+    
+    for (var i in building_types) {
+        var type    = building_types[i];
+        var id      = Math.random().toString().slice(2);
+        var el      = build_el(label({for: id},
+                                     [_input({type: 'checkbox', id: id}),
+                                      type.label]));
+        var input   = el.find('input');
+        
+        input.change(re_draw_buildings);
+        building_types[i].input = input;
+        el.appendTo($('#building_types')); } }
+
+function re_draw_buildings() {
+    var avail_types = [];
+    building_types.map(function(type) {
+        if (type.input.prop('checked'))
+            avail_types.push(type.label); });
+    
+    var data = building_data.filter(function(building) {
+        return member(avail_types, building.predominant_use); });
+    plot_coords_heat(socrata_coords(data), 'buildings'); }
+    
 function init_map() {
     map = L.map('map')
         .setView(kenmore_coords, 13);
-   /* layers.google_satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z=?z',
-                                          {attribution: 'google',
-                                           maxZoon: 20,
-                                           subdomains: ['mt0', 'mt1', 'mt2', 'mt3']});*/
+
     layers.google_satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
         maxZoom:       20,
         subdomains:  ['mt0','mt1','mt2','mt3'],
--- utils.js
+++ utils.js
@@ -177,3 +177,82 @@ function obj_to_urlstring(obj) {
         strs.push(key + "=" + encodeURIComponent(obj[key]));
 
     return strs.join("&"); }
+
+function prepend(el, to) {
+    var chldren = to.childNodes;
+    if (children.length == 0)
+        to.appendChild(el);
+    else
+        to.insertBefore(el, children[0]);
+    return el; }
+
+
+function hash(obj) {
+    if (typeof obj != "string")
+        obj = JSON.stringify(obj); 
+    var hash = 0, i, chr, len;
+    if (obj.length == 0) return hash;
+    for (i = 0, len = obj.length; i < len; i++) {
+        chr   = obj.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer 
+  }
+  return hash;
+}
+
+function tag_maker(tag) {
+    return function(klass, children, click) {
+        click = click || do_nothing;
+        var obj;
+
+        if (typeof klass != "string") 
+            obj = klass;
+        else
+            obj = {"class": klass};
+
+        obj.tag = tag;
+        obj.children = children;
+        obj.click = click;
+        return obj; }; }
+
+var div =    tag_maker('div');
+var span =   tag_maker('span');
+var button = tag_maker('button');
+var a =      tag_maker('a');
+var i =      tag_maker('i');
+var img =    tag_maker('img');
+var strong = tag_maker('strong');
+var p =      tag_maker('p');
+var select = tag_maker('select');
+var _input  = tag_maker('input');
+var option = tag_maker('option');
+var label  = tag_maker('label');
+var textarea = tag_maker('textarea');
+
+function br() {
+    return {tag: 'br'}; }
+
+function build_el(el) {
+    if (el === null) 
+        el = {tag: "div"}; 
+    if (typeof el == "string") 
+        return $(document.createTextNode(el)); 
+
+    var attrs = {};
+    for (var i in el) 
+        if (i != 'tag'
+            && i != 'click'
+            && i != 'children')
+            attrs[i] = el[i];
+
+    var element = $('<' + el.tag + '/>', attrs);
+    if (el.click)
+        element.click(el.click);
+
+    var children = (el.children || [])
+            .map(build_el);
+    for (i in children) 
+        element.append(children[i]);
+
+    return element; }
+
