b02f17209a99fe6b2567310b06c91784d154474e
David Karn <davidkarnseattle@gmail.com>
2015-09-26 11:33:10 -0700

    filter socrata data by kenmore zipcode

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
+var king_county_parcels     = 'yrp3-3hu8';
+var commercial_buildings    = 'b6gg-b3a4';
+var vacant_lots             = 'mdtz-kt2d';
+var employment_stats        = '6bn5-2933';
+var liquor_licenses         = 'mjvz-43ce';
+var kenmore_zip             = 98028;
+
 function lookup(resource, query, next, error) {
-    var query_params = query;
-    var url          = ('https://soda.demo.socrata.com/resource/' + resource
-                        + obj_to_urlstring(query_params));
+    var query_params = {};
+    if (query.where)
+        query_params['$where'] = query.where.join(" AND ");
     
-    $.ajax({url: url,
-            method 'GET'})
+    var url          = ('https://kenmorewa.data.socrata.com/resource/' + resource
+                        + '?' + obj_to_urlstring(query_params));
+    
+    $.ajax({url:      url,
+            method:  'GET'})
         .done(next)
         .fail(error); }
+
+function in_kenmore(query) {
+    query = query || {};
+    query.where = query.where || [];
+    query.where.push("zip_code='" + kenmore_zip + "'");
+    return query; }
+
+function in_kenmore_parcel(query) {
+    query = query || {};
+    query.where = query.where || [];
+    query.where.push('zip_code = ' + kenmore_zip);
+    return query; }
+
+
